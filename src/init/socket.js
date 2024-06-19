8367516d6d21ba7ab92f1dbd900eeff9aab4fd3c
ru2134 <167045410+ru2134@users.noreply.github.com>
2024-06-19 12:00:14 +0900

    Add  socket.js

--- src/init/socket.js
+++ src/init/socket.js
@@ -1,11 +1,29 @@
+// src/init/socket.js
+
 import { Server as SocketIO } from 'socket.io';
 import registerHandler from '../handlers/register.handler.js';
+import { renewalHighScore } from '../handlers/highScore.handler.js';
 
 const initSocket = (server) => {
   const io = new SocketIO();
   io.attach(server);
 
-  registerHandler(io);
+  io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
+
+    
+    registerHandler(socket);
+
+    
+    socket.on('renewalHighScore', async (payload) => {
+      const response = await renewalHighScore(payload);
+      io.emit('highScoreUpdated', response);  
+    });
+
+    socket.on('disconnect', () => {
+      console.log('User disconnected:', socket.id);
+    });
+  });
 };
 
 export default initSocket;
