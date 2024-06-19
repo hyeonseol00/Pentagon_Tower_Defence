f49a4305d2680516e3aa9fff2e4a8230dab2be20
ru2134 <167045410+ru2134@users.noreply.github.com>
2024-06-19 13:40:32 +0900

    Update socket.js

--- src/init/socket.js
+++ src/init/socket.js
@@ -1,27 +1,11 @@
 import { Server as SocketIO } from 'socket.io';
 import registerHandler from '../handlers/register.handler.js';
-import { renewalHighScore } from '../handlers/highScore.handler.js';
 
 const initSocket = (server) => {
   const io = new SocketIO();
   io.attach(server);
 
-  io.on('connection', (socket) => {
-    console.log('A user connected:', socket.id);
-
-    // 기존 핸들러 등록
-    registerHandler(socket);
-
-    // renewalHighScore 이벤트 핸들링
-    socket.on('renewalHighScore', async (payload) => {
-      const response = await renewalHighScore(payload);
-      socket.emit('highScoreUpdated', response);
-    });
-
-    socket.on('disconnect', () => {
-      console.log('User disconnected:', socket.id);
-    });
-  });
+  registerHandler(io);
 };
 
 export default initSocket;
