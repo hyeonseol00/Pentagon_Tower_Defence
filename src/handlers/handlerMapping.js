30614cc44ba2e9c674bf4ba27642e3c8b1d46dca
PSY8937 <qkrtmdduq112@naver.com>
2024-06-19 11:19:19 +0900

    ✨ update : handlerMapping.js
    핸들러매핑 추가

--- src/handlers/handlerMapping.js
+++ src/handlers/handlerMapping.js
@@ -1,7 +1,13 @@
 import { moveStageHandler } from './stage.handler.js';
 
 const handlerMappings = {
+  2: gameStart,
+  3: gameEnd,
   11: moveStageHandler,
+  21: addInitialTowerHandler,
+  22: purchaseTowerHandler,
+  23: monsterKilledHandler,
+  24: monsterAttackedBaseHandler
 };
 
 export default handlerMappings;
