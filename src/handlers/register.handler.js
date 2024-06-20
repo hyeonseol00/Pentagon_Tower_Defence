7407afcb0e4bf44a60782acb3c2349b5fe63fb39
hyeonseol00 <hyeonseol22@naver.com>
2024-06-20 11:01:31 +0900

    ✨ update : 연결 시 쿠키로 토큰 받아 인증

--- src/handlers/register.handler.js
+++ src/handlers/register.handler.js
@@ -1,17 +1,24 @@
-import { v4 as uuidv4 } from 'uuid';
 import { addUser } from '../models/user.model.js';
 import { handleConnection, handleDisconnect, handleEvent } from './helper.js';
+import cookieParser from 'cookie-parser';
+import authMiddleware from '../middlewares/auth.middleware.js';
 
 const registerHandler = (io) => {
-  io.on('connection', async (socket) => {
-    const userUUID = uuidv4();
-    await addUser({ uuid: userUUID, socket_id: socket.id });
+  io.use((socket, next) => {
+    cookieParser()(socket.request, socket.request.res || {}, next);
+  })
+    .use(authMiddleware)
+    .on('connection', async (socket) => {
+      const userID = socket.userId;
+      if (!userID) return;
 
-    handleConnection(socket, userUUID);
+      await addUser({ uuid: userID, socket_id: socket.id });
 
-    socket.on('event', (data) => handleEvent(io, socket, data));
-    socket.on('disconnect', () => handleDisconnect(socket, userUUID));
-  });
+      handleConnection(socket, userID);
+
+      socket.on('event', (data) => handleEvent(io, socket, data));
+      socket.on('disconnect', () => handleDisconnect(socket, userID));
+    });
 };
 
 export default registerHandler;
