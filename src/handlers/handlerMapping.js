ab5635c3aa6b4bdbe7fd7e75b325a83752a03f1f
PSY8937 <qkrtmdduq112@naver.com>
2024-06-20 18:40:12 +0900

    🩹 fix : handlerMapping.js
    주석처리 제거 및 각 핸들러 모듈 가져오기

--- src/handlers/handlerMapping.js
+++ src/handlers/handlerMapping.js
@@ -1,11 +1,15 @@
+import { gameStart, gameEnd } from "./game.handler.js"
+import { addInitialTowerHandler, purchaseTowerHandler  } from "./tower.handler.js";
+import { monsterKilledHandler } from "./monster.handler.js"
+import { monsterAttackedBaseHandler } from "./base.handler.js"
+
 const handlerMappings = {
-  /* 
   2: gameStart,
   3: gameEnd,
   21: addInitialTowerHandler,
   22: purchaseTowerHandler,
   23: monsterKilledHandler,
-  24: monsterAttackedBaseHandler, */
+  24: monsterAttackedBaseHandler,
 };
 
 export default handlerMappings;
