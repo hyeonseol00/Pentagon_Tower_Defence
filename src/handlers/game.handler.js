167b99b8a977baa527dfb7280c08996d088f1bf3
ru2134 <167045410+ru2134@users.noreply.github.com>
2024-06-20 11:43:19 +0900

    🔥 del : 미사용 코드 정리

---
+++ src/handlers/game.handler.js
@@ -0,0 +1,36 @@
+import { getGameAssets } from '../init/assets.js';
+import { getHighScore, updateHighScore } from '../models/high-score.model.js';
+import { getUserData, updateUserData } from '../models/user-data.model.js';
+
+export const gameStart = (userId, payload) => {
+  return { status: 'success', message: '게임이 정상적으로 실행되었습니다!.' };
+};
+
+export const gameEnd = async (userId, payload) => {
+  const userData = await getUserData(userId);
+
+  if (Math.abs(userData.score - payload.score) >= 200)
+    return { status: 'fail', message: '점수 데이터가 잘못되었습니다!' };
+
+  if (userData.user_high_score < payload.score)
+    userData.user_high_score = payload.score;
+
+  await updateUserData(userData);  
+
+  const highScore = await getHighScore();
+
+  if (highScore < payload.score) {
+    await updateHighScore(payload.score);
+    return {
+      status: 'success',
+      message: '게임 종료, 최고기록이 갱신되었습니다!',
+      broadcast: {
+        message: '서버 최고기록이 갱신되었습니다!',
+        userId,
+        highscore: highScore,
+      },
+    };
+  }
+
+  return { status: 'success', message: '게임 종료!', data: userData };
+};
